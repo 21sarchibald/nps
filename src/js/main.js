0603c0f8806d7d5dcfa969d28258ac4aaac6bad1
Sophie Archibald <arc21004@byui.edu>
2025-02-27 09:56:57 -0700

    Created setInfo function

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,38 @@ import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
 
+function setHeaderInfo(data) {
+
 const title = document.querySelector("head > title");
-title.innerHTML = parkData.fullName;
+title.innerHTML = data.fullName;
 
 const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
+disclaimer.href = data.url;
+disclaimer.innerHTML = data.fullName;
+}
+
+setHeaderInfo(parkData);
 
 function parkInfoTemplate(info) {
-    return `<a href="/" class="hero-title">${info.name}</a>
+    return `<h1><a href="/" class="hero-title">${info.name}</a></h1>
     <p class="hero-subtitle">
         <span>${info.designation}</span>
         <span>${info.states}</span>
     </p>`;
 }
 
-const parkTemplate = document.querySelector("#hero-text");
+let parkTemplate = document.querySelector("#hero-text");
 
-parkTemplate = parkInfoTemplate(parkData);
+parkTemplate.innerHTML = parkInfoTemplate(parkData);
 
 const parkImage = document.querySelector("#park-hero > img");
 parkImage.src = parkData.images[0].url;
 parkImage.alt = parkData.images[0].altText;
+
+const parkIntro = document.querySelector(".intro");
+
+function setInfo(data) {
+    return `<h1 id="parkName">${data.fullName}</h1>
+        <p id="parkDescription">${data.description}</p>`
+}
+document.querySelector(".info").innerHTML = setInfo(parkData);
