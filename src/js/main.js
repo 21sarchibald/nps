4ddd21450ea162a1dfdea915f4cca7ab694e0a8f
Sophie Archibald <arc21004@byui.edu>
2025-02-28 13:09:03 -0700

    Inserted footer html

--- src/js/main.js
+++ src/js/main.js
@@ -30,10 +30,72 @@ const parkImage = document.querySelector("#park-hero > img");
 parkImage.src = parkData.images[0].url;
 parkImage.alt = parkData.images[0].altText;
 
-const parkIntro = document.querySelector(".intro");
 
 function setInfo(data) {
     return `<h1 id="parkName">${data.fullName}</h1>
         <p id="parkDescription">${data.description}</p>`
 }
 document.querySelector(".info").innerHTML = setInfo(parkData);
+
+function mediaCardTemplate(info) {
+    return `
+    <div>
+    <img src="${info.image}" alt="">
+    <h2><a href="${info.link}">${info.name}</a></h2>
+    <p>${info.description}</p>
+    </div>
+    `;
+}
+
+const parkInfoLinks = [
+    {
+      name: "Current Conditions &#x203A;",
+      link: "conditions.html",
+      image: parkData.images[2].url,
+      description:
+        "See what conditions to expect in the park before leaving on your trip!"
+    },
+    {
+      name: "Fees and Passes &#x203A;",
+      link: "fees.html",
+      image: parkData.images[3].url,
+      description: "Learn about the fees and passes that are available."
+    },
+    {
+      name: "Visitor Centers &#x203A;",
+      link: "visitor_centers.html",
+      image: parkData.images[9].url,
+      description: "Learn about the visitor centers in the park."
+    }
+  ];
+
+const infoSection = document.querySelector(".info");
+parkInfoLinks.map(section => infoSection.innerHTML += mediaCardTemplate(section));
+
+
+function getMailingAddress(addresses) {
+    const mailing = addresses.find((address) => address.type === "Mailing");
+    return mailing;
+  }
+
+function getVoicePhone(phoneNumbers) {
+    const phoneNumber = phoneNumbers.find((phoneNumber) => phoneNumber.type === "Voice");
+    return phoneNumber.phoneNumber;
+}
+
+function footerTemplate(info) {
+    console.log("running");
+    const mailing = getMailingAddress(info.addresses);
+    const voice = getVoicePhone(info.contacts.phoneNumbers)
+    
+    return `<section class="contact">
+    <h3>Contact Info</h3>
+    <h4>Mailing Address:</h4>
+    <div><p>${mailing.line1}<p>
+    <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p></div>
+    <h4>Phone:</h4>
+    <p>${voice}</p>
+    </section>`;
+  }
+
+document.querySelector("#park-footer").innerHTML = footerTemplate(parkData);
