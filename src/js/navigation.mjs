3256ba815e579f41d57624035d0ed34ac3cdb6ca
Sophie Archibald <arc21004@byui.edu>
2025-03-26 16:02:29 -0600

    Added global nav to conditions html

--- src/js/navigation.mjs
+++ src/js/navigation.mjs
@@ -1,31 +1,35 @@
+function buildMainMenu(ev) {
+    const globalNav = document.querySelector(".global-nav");
+    console.log(globalNav);
+    let target = ev.target;
+    globalNav.classList.toggle("show");
+
+    if (!target.matches("button")) {
+            target = target.closest("button");
+    }
+
+    if (globalNav.classList.contains("show")) {
+        target.setAttribute("aria-expanded", "true");
+    }
+
+    else {
+        target.setAttribute("aria-expanded", "false");
+    }
+
+}
+
+function buildSubmenu(ev) {
+    ev.currentTarget.closest("li").querySelector(".global-nav-sublist").classList.toggle("show");
+    ev.currentTarget.querySelector(".icon").classList.toggle("rotate");
+}
+
 export function enableNavigation() {
 
     const menuButton = document.querySelector("#global-nav-toggle");
-    const globalNav = document.querySelector(".global-nav");
-
-    menuButton.addEventListener("click", (ev) => {
-        let target = ev.target;
-        globalNav.classList.toggle("show");
-
-        if (!target.matches("button")) {
-            target = target.closest("button");
-        }
-
-        if (globalNav.classList.contains("show")) {
-            target.setAttribute("aria-expanded", "true");
-        }
-
-        else {
-            target.setAttribute("aria-expanded", "false");
-        }
-
-    });
+    menuButton.addEventListener("click", buildMainMenu);
 
     const openSubListButtons = document.querySelectorAll(".global-nav__split-button__toggle");
     openSubListButtons.forEach((button) => {
-        button.addEventListener("click", (ev) => {
-            ev.currentTarget.closest("li").querySelector(".global-nav-sublist").classList.toggle("show");
-            ev.currentTarget.querySelector(".icon").classList.toggle("rotate");
-        });
+        button.addEventListener("click", buildSubmenu);
     });
 }
