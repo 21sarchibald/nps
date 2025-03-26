5ca59a19512432e974a844ab91bff0f0def6e12c
Sophie Archibald <arc21004@byui.edu>
2025-03-25 21:50:28 -0600

    Added animation

--- src/js/main.js
+++ src/js/main.js
@@ -64,6 +64,13 @@ function enableNavigation() {
 
     });
 
+    const openSubListButtons = document.querySelectorAll(".global-nav__split-button__toggle");
+    openSubListButtons.forEach((button) => {
+        button.addEventListener("click", (ev) => {
+            ev.currentTarget.closest("li").querySelector(".global-nav-sublist").classList.toggle("show");
+            ev.currentTarget.querySelector(".icon").classList.toggle("rotate");
+        });
+    });
 }
 
 
